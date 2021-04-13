83ff621799483a0081eea65391767ce2afd17430
Cole Fleming <>
2021-04-13 03:16:45 +0000

    Fixed Start Script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 read -p "Do You Want A Config File? (Y|N): " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    git clone "https://github.com/derpferpmerp/HangMan-Attempt-2021.git";cd "HangMan-Attempt-2021";sudo chmod 777 *;mv main.py ./HANGMAN.py;sudo pip3 install inquirer;echo "Run 'python3 ./HANGMAN.py' to Start";exit 1
+    git clone "https://github.com/derpferpmerp/HangMan-Attempt-2021.git"&&cd "HangMan-Attempt-2021";sudo chmod 777 *;mv main.py ./HANGMAN.py;sudo pip3 install inquirer;echo "Run 'python3 ./HANGMAN.py' to Start";exit 1
 fi
 mkdir HANGMAN;cd HANGMAN;curl -o HANGMAN.py "https://raw.githubusercontent.com/derpferpmerp/HangMan-Attempt-2021/master/main.py"||wget -O HANGMAN.py "https://raw.githubusercontent.com/derpferpmerp/HangMan-Attempt-2021/master/main.py"||echo "Download Unsuccessful. Please Install wget or curl";exit 1
 sudo pip3 install inquirer;echo "Run 'python3 HANGMAN.py' to Start";
